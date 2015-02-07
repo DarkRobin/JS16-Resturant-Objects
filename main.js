de0c438d9cabb050326f2c4e850f8cb231a60811
charles <sam.charles.harrod@gmail.com>
2015-02-06 17:08:13 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+
+
+
+var Food = function(name, calories, vegan, gluten, citrus){
+	this.name = name;
+	this.calories = calories;
+	this.vegan = vegan;
+	this.gluten = gluten;
+	this.citrus = citrus;
+};
+
+Food.prototype.toString = function(){
+	console.log('Dish name: ' + this.name + 'Calories: ' + this.calories + 'Vegan?: ' + this.vegan + 'Gluten?: ' + );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+Food('taco', 100, false, false, false);
+
+
+
+
+
+
+
+
+
+
+$(document).on('ready', function() {
+  
+});
